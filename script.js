62fa8a168d7d67ec2a547f66207eaa47f59c35e6
Swathi <swathitirupathi2001@gmail.com>
2022-11-28 18:31:49 +0530

    made changes

--- script.js
+++ script.js
@@ -4,14 +4,15 @@ const scissors = document.getElementById("scissors");
 const user = document.getElementById("user");
 const comp = document.getElementById("witch");
 const result = document.getElementById("result");
+const f_result = document.getElementById("f_result");
 
 rock.addEventListener("click", () => playRound("rock"));
 paper.addEventListener("click", () => playRound("paper"));
 scissors.addEventListener("click", () => playRound("scissors"));
 
-let user_score =0,comp_score=0;
+let user_score = 0, comp_score = 0;
 
-function proceedGame() {
+function endGame() {
     return (user_score === 5 || comp_score === 5);
 }
 function getComputerChoice() {
@@ -21,24 +22,31 @@ function getComputerChoice() {
 }
 function playRound(playerSelection) {
     // your code here!
-    if(proceedGame()){
-        if(user_score===5){
-            prompt("You won");
-        }
-        else{
-            prompt("Witch won");
-        }
-    }
-
     let computerSelection = getComputerChoice();
     if (playerSelection === computerSelection) {
+        result.innerHTML = "It's a tie!";
     }
     else if ((playerSelection == "rock" && computerSelection == "scissors") || (playerSelection == "paper" && computerSelection == "rock") || (playerSelection == "scissors" && computerSelection == "paper")) {
-        user_score +=1;
+        user_score += 1;
         user.innerHTML = user_score;
+        result.innerHTML = "You scored 1 point!";
     }
     else {
         comp_score += 1;
         witch.innerHTML = comp_score;
+        result.innerHTML = "Witch scored 1 point!";
+    }
+    message();
+}
+function message() {
+    if (endGame()) {
+        if (user_score > comp_score) {
+            result.innerHTML = "You won! \n Get your Potion!!!";
+        }
+        else {
+            result.innerHTML = "Witch won";
+        }
     }
 }
+
+
